78370c782fc58b96fc8de432626d94cc82df80b1
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-30 15:47:51 +0300

    feat: added a custom input component & managed modal visibility

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -19,10 +19,15 @@ const Cart = () => {
         userProgressContext.hideCart();
     }
 
+    function handleGoToCheckout() {
+        userProgressContext.showCheckout();
+    }
+
     return (
         <Modal
             className="cart"
             open={userProgressContext.progress === 'cart'}
+            onClose={userProgressContext.progress === 'cart'? handleCloseCart : null}
         >
             <h2>Your Cart</h2>
             <ul>
@@ -40,7 +45,9 @@ const Cart = () => {
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
                 <Button textOnly onClick={handleCloseCart}>Close</Button>
-                <Button>Go to Checkout</Button>
+                {cartContext.items.length > 0 && (
+                    <Button onClick={handleGoToCheckout}>Go to Checkout</Button>
+                )}
             </p>
         </Modal>
     );
